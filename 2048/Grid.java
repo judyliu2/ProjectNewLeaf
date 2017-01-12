196af17b64be2f29d5bf11a367241b9d8b487199
Carol217 <cpan3@stuy.edu>
2017-01-12 08:44:55 -0500

    Grid outline

---
+++ 2048/Grid.java
@@ -0,0 +1,49 @@
+public class Grid<t>{
+
+    //~~~~~~~~~~~~~~Instance Vars~~~~~~~~~~~~~~~~~~~~~
+    private t[][] userGrid;
+    private int placesFilled;
+    private int maxPlaces;
+    private t nullValue;
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    //~~~~~~~~~~~~~~~constructor~~~~~~~~~~~~~~~~~~~~~~
+    public Grid(int row, int column, t nullVal){
+
+    }
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    //~~~~~~~~~~~~~~~~~methods~~~~~~~~~~~~~~~~~~~~~~~
+    public String toString(){
+
+	return "";
+    }
+
+    public void remove(int i, int j){
+	
+    }
+
+    public void set(int i, int j, t object){
+
+    }
+
+    public void leftJustify(){
+
+    }
+
+    public void rightJustify(){
+
+    }
+
+    public void upJustify(){
+
+    }
+
+    public void downJustify(){
+
+    }
+    
+    public static void main(String[] args){
+
+    }
+}
